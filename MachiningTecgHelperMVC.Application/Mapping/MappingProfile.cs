8ca58a2007d18ec963bdb2397da0ae7d0e62e2b6
grzkarpinski <66385734+grzkarpinski@users.noreply.github.com>
2024-02-21 16:51:37 +0100

    add PappingProfle class

--- MachiningTecgHelperMVC.Application/Mapping/MappingProfile.cs
+++ MachiningTecgHelperMVC.Application/Mapping/MappingProfile.cs
@@ -2,12 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MachiningTecgHelperMVC.Application.Mapping
 {
+    // auto mapper profile configuration
+    // this class is used to autmate the mapping of the domain model to the view model
     public class MappingProfile: Profile
     {
+        public MappingProfile() 
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces()
+                                   .Where(i => i.IsGenericType)
+                                                      .Any(i => i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping") ?? type.GetInterface("IMapFrom`1").GetMethod("Mapping");
+                methodInfo.Invoke(instance, new object[] { this });
+            }
+        }
     }
 }
