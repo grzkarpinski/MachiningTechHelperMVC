8dbb51ddd7d16b3eddd1aa34580abce01ae2f246
grzkarpinski <66385734+grzkarpinski@users.noreply.github.com>
2024-04-17 17:20:50 +0200

    add tests to calculators

---
+++ MachiningTechHelperMVC.Tests/CostEstimationTests.cs
@@ -0,0 +1,27 @@
+﻿using MachiningTechelperMVC.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachiningTechHelperMVC.Tests
+{
+    public class CostEstimationTests
+    {
+        [Fact]
+        public void TimeCostsTest()
+        {
+            // Arrange
+            var machineType = new List<string> { "Grupa1", "Grupa17" };
+            var times = new List<decimal?> { 10, 100 };
+            var expected = new List<decimal?> { 18.33m, 150.00m };
+
+            // Act
+            var result = new CostEstimationLogic().TimeCosts(machineType, times);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ MachiningTechHelperMVC.Tests/SimleCalculatorLogicTests.cs
@@ -0,0 +1,55 @@
+﻿using MachiningTechHelperMVC.Domain.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachiningTechHelperMVC.Tests
+{
+    public class SimleCalculatorLogicTests
+    {
+        [Fact]
+        public void CalculateRevitonsPerMinuteTest()
+        {
+            // Arrange
+            var logic = new SimpleCalculatorLogic();
+            double cuttingSpeed = 100;
+            double diameter = 10;
+            double expected = 3183.098861837907;
+            // Act
+            double actual = logic.CalculateRevitonsPerMinute(cuttingSpeed, diameter);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CalculateMillingFeedTest()
+        {
+            // Arrange
+            var logic = new SimpleCalculatorLogic();
+            double feedPerTooth = 0.1;
+            int teeth = 2;
+            double revolutionsPerMinute = 1000;
+            double expected = 200;
+            // Act
+            double actual = logic.CalculateMillingFeed(feedPerTooth, teeth, revolutionsPerMinute);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CalculateDrillingFeedTest()
+        {
+            // Arrange
+            var logic = new SimpleCalculatorLogic();
+            double feedPerRevolution = 0.1;
+            double revolutionsPerMinute = 1000;
+            double expected = 100;
+            // Act
+            double actual = logic.CalculateDrillingFeed(feedPerRevolution, revolutionsPerMinute);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
