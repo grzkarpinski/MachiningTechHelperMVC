9152b39b3c3b01361ee5b2def64d9713a3720bc4
grzkarpinski <66385734+grzkarpinski@users.noreply.github.com>
2024-05-26 18:42:58 +0200

    Added user management features to admin panel

---
+++ MachiningTecgHelperMVC.Application/ViewModels/User/UserDetailsVm.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachiningTechelperMVC.Application.ViewModels.User
+{
+    public class UserDetailsVm
+    {
+        public IdentityUser User { get; set; }
+        public IList<string> Roles { get; set; }
+        public IList<string> AllRoles { get; set; }
+        public string SelectedRole { get; set; }
+    }
+}
--- MachiningTechHelperMVC.Web/Controllers/AdminController.cs
+++ MachiningTechHelperMVC.Web/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using MachiningTechelperMVC.Application.ViewModels.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MachiningTechHelperMVC.Web.Controllers
@@ -79,7 +80,82 @@ namespace MachiningTechHelperMVC.Web.Controllers
             return View("Roles", _roleManager.Roles);
         }
 
-        //TODO: Add methods for managing users
+        //DONE. Add search functionality?
 
+        public IActionResult Users()
+        {
+            return View(_userManager.Users);
+        }
+
+        //DONE. TEST THIS!
+
+        public IActionResult DeleteUser(string id)
+        {
+            IdentityUser user = _userManager.FindByIdAsync(id).Result;
+            if (user != null)
+            {
+                IdentityResult result = _userManager.DeleteAsync(user).Result;
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Users");
+                }
+            }
+            return View("Users", _userManager.Users);
+        }
+
+        //DONE
+
+        public async Task<IActionResult> UserDetails(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.Select(r => r.Name).ToList();
+                var model = new UserDetailsVm
+                {
+                    User = user,
+                    Roles = roles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Users");
+            }
+        }
+
+        //DONE
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(string id, UserDetailsVm model)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null && !string.IsNullOrEmpty(model.SelectedRole))
+            {
+                var result = await _userManager.AddToRoleAsync(user, model.SelectedRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UserDetails", new { id = user.Id });
+                }
+            }
+            return RedirectToAction("Users");
+        }
+
+        //DONE
+        [HttpPost]
+        public async Task<IActionResult> RemoveRole(string id, string role)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var result = await _userManager.RemoveFromRoleAsync(user, role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UserDetails", new { id = user.Id });
+                }
+            }
+            return RedirectToAction("Users");
+        }
     }
 }
