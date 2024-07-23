8eae833dd0787bb7c07179903d8c41355d11610d
grzkarpinski <66385734+grzkarpinski@users.noreply.github.com>
2024-07-23 16:15:18 +0200

    Refactor ISimpleCalculatorLogic interface
    delete not used code

--- MachiningTecgHelperMVC.Application/Interfaces/ISimpleCalculatorLogic.cs
+++ MachiningTecgHelperMVC.Application/Interfaces/ISimpleCalculatorLogic.cs
@@ -10,8 +10,5 @@ namespace MachiningTechelperMVC.Application.Interfaces
     public interface ISimpleCalculatorLogic
     {
         SimpleCalculatorVm Calculate(SimpleCalculatorVm model);
-		//double CalculateRevitonsPerMinute(double cuttingSpeed, double diameter);
-  //      double CalculateMillingFeed(double feedPerTooth, int teeth, double revolutionsPerMinute);
-  //      double CalculateDrillingFeed(double feedPerRevolution, double revolutionsPerMinute);
     }
 }
